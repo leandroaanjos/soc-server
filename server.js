e7f36f1ef97b37a38cfe4b42c698a561fa56be16
Leandro A Dos Anjos <leandro.anjos@gsoftware.com.br>
2021-09-23 22:32:14 -0300

    Adicionado arquivos dos server

---
+++ server.js
@@ -0,0 +1,22 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3003;
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// Add this before server.use(router)
+// server.use(
+//   jsonServer.rewriter({
+//     "/api/*": "/$1",
+//   })
+// );
+
+// Use default router
+server.use('/api/v1', router);
+
+server.listen(port, () => {
+  console.log(`JSON Server is running. http://localhost:${port}`);
+});
